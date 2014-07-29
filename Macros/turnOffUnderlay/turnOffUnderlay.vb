d81f6688a4de4e66f569f79794c635167b5a5bdf
Brett Burling <brett.burling@gmail.com>
2014-07-30 08:14:47 +1000

    VB TurnOffUnderlay Added

---
+++ Macros/turnOffUnderlay/turnOffUnderlay.vb
@@ -0,0 +1,35 @@
+'this macro will collect all the views and then loop through them turning off the underlay in each one		
+Public Sub TurnOffUnderlay()
+		
+		'assigns document to the active document in use
+		Dim document As Document = Me.ActiveUIDocument.Document
+		
+		'get all the views
+		Dim viewCollection As ienumerable(Of view) = From v As View In New FilteredElementCollector(document) _
+											.OfCategory(BuiltInCategory.OST_Views) _
+											.whereelementisnotelementtype _
+											.cast(Of view)
+		
+		'Start a transaction CRITICAL without transactions Revit cannot update
+		Using rvtTransaction As New Transaction(document,"comment updates")
+		  			rvtTransaction.Start()
+		
+
+		'loop through the views												  									
+		For Each v As View In viewCollection
+			
+			'get the underlay parameter
+			Dim p As Parameter = v.Parameter(BuiltInParameter.VIEW_UNDERLAY_ID)
+			
+			'if parameter exists, set it to nothing
+			If p IsNot Nothing Then
+				p.Set(New ElementId(-1))
+			End If
+		Next
+		
+		'Commit the changes to the Revit File 
+		rvtTransaction.Commit
+	End Using
+
+	
+End Sub
